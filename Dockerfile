748c44346d06e6f88d1813ae1b992fdb7a9ad9f8
Pedro Henrique <107046565+pedrosantos4@users.noreply.github.com>
2025-06-17 20:13:28 -0300

    First version

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:latest
+WORKDIR /api
+COPY . .
+RUN rm -rf node_modules
+RUN npm i
+CMD ["npm", "start"]
+EXPOSE 3000
---
+++ api/src/handler.js
@@ -0,0 +1,40 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+const app = express();
+
+app.get('/urlshortener', (req, res) => {
+    res.send({
+        status: 'ok'
+    });
+});
+
+
+app.listen(3000);
+console.log("running...");
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
