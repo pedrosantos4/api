e1ce8db74069e8639d86878d2c296c863f2befed
Pedro Henrique <107046565+pedrosantos4@users.noreply.github.com>
2025-06-18 13:17:54 -0300

    Update API
    Adicionado verificação de erros no body da requisição, campos com valores vazios ou ausentes, também adicionado um validador de conexão no banco de de consulta

--- api/src/handler.js
+++ api/src/handler.js
@@ -1,11 +1,12 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
-import express from "express"
-import dotenv from "dotenv"
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import express from 'express';
+import dotenv from 'dotenv';
+import { codeGenerator, validateRequiredFieldsValues, validateRequiredFields } from './utils/functions.js';
+
 dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -13,28 +14,68 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-const app = express();
 
-app.get('/urlshortener', (req, res) => {
-    res.send({
-        status: 'ok'
+const app = express();
+app.use(express.json());
+
+let isConnected = false;
+const requiredFields = ['db', 'collection', 'query'];
+
+
+app.post('/urlshortener', async (req, res) => {
+
+  const missingFields = validateRequiredFields(req.body, requiredFields);
+  if (missingFields.missing) {
+    return res.status(400).send({
+      status: 'error',
+      message: `Missing fields: '${missingFields.fields.join(', ')}'`
     });
+  }
+
+  const missingFieldsValue = validateRequiredFieldsValues(req.body, requiredFields);
+  if (missingFieldsValue.missing) {
+    return res.status(400).send({
+      status: 'error',
+      message: `Missing value in fields: '${missingFieldsValue.fields.join(', ')}'`
+    });
+  }
+
+  if (!isConnected) {
+    try {
+      await client.connect();
+      isConnected = true;
+      console.log("MongoDB connected");
+    } catch (error) {
+      res.status(500).send({ status: 'error', message: error.message });
+    }
+  }
+
+  const db = client.db(req.body.db);
+  const collection = db.collection(req.body.collection);
+  const query = req.body.query;
+  try {
+    const result = await collection.findOne(query);
+
+    //console.log(db, collection, query)
+    console.log(result)
+    if (result && result.urlShortened) {
+      res.send({
+        status: 'ok',
+        data: {
+          "urlShortened": result.urlShortened
+        }
+      });
+    } else {
+      res.status(404).send({
+        status: 'error',
+        message: 'Not Found'
+      });
+    }
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: 'error', message: error.message });
+  }
 });
 
-
-app.listen(3000);
-console.log("running...");
-
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+app.listen(5002, () => console.log("running..."));
